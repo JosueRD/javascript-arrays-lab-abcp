01dabcdeaaf6797d05c889e4314b8014ef62e045
Josue Rios <josuerd89@gmail.com>
2018-01-18 03:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function  kittens(){
   var kittens=["Milo", "Otis", "Garfield"]
   console.log(kittens)
 }
+function destructivelyAppendKitten(name){
+  var kittens
+  kittens.push(name)
+  console.log(kittens)
+}
